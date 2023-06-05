84132a191e1f4c042eb9e73e4c832275a25d691c
Wendel Reis <wendel.m.w18@gmail.com>
2023-06-05 20:01:55 -0300

    container message

--- Project JS/main.js
+++ Project JS/main.js
@@ -12,17 +12,19 @@ form.addEventListener('submit', function(e) {
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} deposito para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> deposito para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
 
     formEValido = validaNome(nomeBeneficiario.value)
     if (formEValido) {
-        alert(mensagemSucesso);
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
 
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
     } else {
-        alert("O nome não está completo");
+        document.querySelector('.error-message').style.display = 'block';
     }
 })
 
