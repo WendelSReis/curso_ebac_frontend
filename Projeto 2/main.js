6ee1ef6f73f902679d0cbdb898cd753570fa24c6
WendelSReis <wendel.m.w18@gmail.com>
2023-08-11 20:17:10 -0300

    Limpando linhas e add emojis

--- Projeto 2/main.js
+++ Projeto 2/main.js
@@ -1,4 +1,7 @@
 const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+
 let linhas = '';
 
 form.addEventListener('submit', function(e){
@@ -10,11 +13,14 @@ form.addEventListener('submit', function(e){
     let linha = '<tr>';
     linha += `<tr>${inputNomeAtividade.value}</tr>`;
     linha += `<tr>${inputNotaAtividade.value}</tr>`;
-    linha += `<tr>${inputNotaAtividade >= 7 ? 'Aprovado' : 'Reprovado'}</tr>`;
+    linha += `<tr>${inputNotaAtividade >= 7 ? imgAprovado : imgReprovado}</tr>`;
     linha += '</tr>';
 
     linhas += linha;
 
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
 });
