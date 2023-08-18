8ce97f920df41ec7a1431b35e0768ff5846c41b2
WendelSReis <wendel.m.w18@gmail.com>
2023-08-18 20:04:11 -0300

    Organizando JS

--- Projeto 2/main.js
+++ Projeto 2/main.js
@@ -1,14 +1,24 @@
 const form = document.getElementById('form-atividade');
 const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+const atividades = [];
+const notas = [];
 
 let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    atividades.push(inputNomeAtividade.value);
+    notas.push(inputNotaAtividade.value);
 
     let linha = '<tr>';
     linha += `<tr>${inputNomeAtividade.value}</tr>`;
@@ -18,9 +28,11 @@ form.addEventListener('submit', function(e){
 
     linhas += linha;
 
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
-});
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
