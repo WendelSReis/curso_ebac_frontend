eb3b05439194b90eb12c9490708132ed02c52e02
WendelSReis <wendel.m.w18@gmail.com>
2023-08-22 21:35:54 -0300

    Add JS

---
+++ Agenda de contatos/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contato-form');
+    const tabelaContatos = document.getElementById('contatos');
+    const cadastrarBtn = document.getElementById('cadastrar');
+
+});
